64e33574bd4b1af8129560b757260733a070732f
mcgear <michael.gearhardt@gmail.com>
2021-05-03 18:14:41 -0600

    Adjusted missing lcu imports

--- projects/lcu/src/app/app.module.ts
+++ projects/lcu/src/app/app.module.ts
@@ -1,17 +1,34 @@
 import { DoBootstrap } from '@angular/core';
 import { Injector } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
-import { ApplicationsFlowProjectsElementComponent } from '@lowcodeunit/applications-flow-common';
+import {
+  ApplicationsFlowModule,
+  ApplicationsFlowProjectsElementComponent,
+} from '@lowcodeunit/applications-flow-common';
 import { SELECTOR_APPLICATIONS_FLOW_PROJECTS_ELEMENT } from '@lowcodeunit/applications-flow-common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FathymSharedModule, LCUServiceSettings } from '@lcu/common';
+import {
+  FathymSharedModule,
+  LCUServiceSettings,
+  MaterialModule,
+} from '@lcu/common';
 import { environment } from '../environments/environment';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppHostModule } from '@lowcodeunit/app-host-common';
 
 @NgModule({
   declarations: [],
-  imports: [BrowserModule, BrowserAnimationsModule, FathymSharedModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FathymSharedModule.forRoot(),
+    MaterialModule,
+    FlexLayoutModule,
+    AppHostModule.forRoot(),
+    ApplicationsFlowModule.forRoot(),
+  ],
   providers: [
     {
       provide: LCUServiceSettings,
