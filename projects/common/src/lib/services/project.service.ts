c1c7620f540dcfebbc118cec6ebad1c110846566
mcgear <michael.gearhardt@gmail.com>
2021-07-07 11:58:11 -0600

    Formatting

--- projects/common/src/lib/services/project.service.ts
+++ projects/common/src/lib/services/project.service.ts
@@ -2,65 +2,67 @@ import { Injectable } from '@angular/core';
 import { BaseModeledResponse, BaseResponse } from '@lcu/common';
 import { Subject, Subscription } from 'rxjs';
 import { ProjectItemModel } from '../models/project-item.model';
-import { ApplicationsFlowState, GitHubWorkflowRun, ProjectState } from '../state/applications-flow.state';
+import {
+  ApplicationsFlowState,
+  GitHubWorkflowRun,
+  ProjectState,
+} from '../state/applications-flow.state';
 import { ApplicationsFlowService } from './applications-flow.service';
 
 @Injectable({
-    providedIn: 'root',
-  })
+  providedIn: 'root',
+})
+export class ProjectService {
+  public CreatingProject: boolean;
 
+  public CurrentSelectedProject: Subject<ProjectState>;
 
-  export class ProjectService {
+  /**
+   * Subject for editing project settings
+   */
+  public SetEditProjectSettings: Subject<ProjectItemModel>;
 
-    public CreatingProject: boolean;
+  constructor(protected appsFlowSvc: ApplicationsFlowService) {
+    this.SetEditProjectSettings = new Subject<ProjectItemModel>();
+    this.CurrentSelectedProject = new Subject<ProjectState>();
+  }
 
-    public CurrentSelectedProject: Subject<ProjectState>;
+  public DeployRun(state: ApplicationsFlowState, run: GitHubWorkflowRun): void {
+    state.Loading = true;
 
-    /**
-     * Subject for editing project settings
-     */
-    public SetEditProjectSettings: Subject<ProjectItemModel>;
+    this.appsFlowSvc.DeployRun(run).subscribe((response: BaseResponse) => {
+      if (response.Status.Code === 0) {
+        this.ListProjects(state);
+      } else {
+        state.Loading = false;
+      }
+    });
+  }
 
-    constructor(protected appsFlowSvc: ApplicationsFlowService) {
-        this.SetEditProjectSettings = new Subject<ProjectItemModel>();
-        this.CurrentSelectedProject = new Subject<ProjectState>();
+  public ListProjects(
+    state: ApplicationsFlowState,
+    withLoading: boolean = true
+  ): void {
+    if (withLoading) {
+      state.Loading = true;
     }
 
-    public DeployRun(state: ApplicationsFlowState, run: GitHubWorkflowRun): void {
-  
-      state.Loading = true;
-  
-      this.appsFlowSvc.DeployRun(run).subscribe((response: BaseResponse) => {
+    this.appsFlowSvc
+      .ListProjects()
+      .subscribe((response: BaseModeledResponse<ProjectState[]>) => {
         if (response.Status.Code === 0) {
-          this.ListProjects(state);
-        } else {
+          state.Projects = response.Model;
+        } else if (response.Status.Code === 3) {
+        }
+
+        if (withLoading) {
           state.Loading = false;
         }
-      });
-    }
 
-    public ListProjects(state: ApplicationsFlowState, withLoading: boolean = true): void {
-      if (withLoading) {
-        state.Loading = true;
-      }
-  
-      this.appsFlowSvc
-        .ListProjects()
-        .subscribe((response: BaseModeledResponse<ProjectState[]>) => {
-          if (response.Status.Code === 0) {
-            state.Projects = response.Model;
-          } else if (response.Status.Code === 3) {
-          }
-  
-          if (withLoading) {
-            state.Loading = false;
-          }
-  
-          this.CreatingProject =
-            !state.Projects || state.Projects.length <= 0;
-  
-          this.appsFlowSvc.UpdateState(state);
-          console.log(state);
-        });
-    }
+        this.CreatingProject = !state.Projects || state.Projects.length <= 0;
+
+        this.appsFlowSvc.UpdateState(state);
+        console.log(state);
+      });
   }
+}
