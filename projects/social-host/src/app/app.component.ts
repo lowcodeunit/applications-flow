b4627bbb493fe15f683a33d50d03a904198a7c48
mcgear <michael.gearhardt@gmail.com>
2022-01-24 09:55:57 -0700

    must call ensureuserenterprise separately

--- projects/social-host/src/app/app.component.ts
+++ projects/social-host/src/app/app.component.ts
@@ -6,23 +6,22 @@ import { ProjectService } from 'projects/common/src/lib/services/project.service
 @Component({
   selector: 'lcu-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'social-host';
 
   public State: ApplicationsFlowState;
 
-  constructor(protected serviceSettings: LCUServiceSettings,
-    protected projectService: ProjectService) {
+  constructor(
+    protected serviceSettings: LCUServiceSettings,
+    protected projectService: ProjectService
+  ) {
     this.State = new ApplicationsFlowState();
-
   }
 
   public ngOnInit(): void {
-
     this.handleStateChange().then((eac) => {});
-    
   }
 
   protected async handleStateChange(): Promise<void> {
@@ -30,15 +29,12 @@ export class AppComponent {
 
     await this.projectService.HasValidConnection(this.State);
 
+    await this.projectService.EnsureUserEnterprise(this.State);
+
     await this.projectService.ListEnterprises(this.State);
 
     if (this.State.Enterprises?.length > 0) {
-      
       await this.projectService.GetActiveEnterprise(this.State);
-
     }
-
   }
 }
-
-
