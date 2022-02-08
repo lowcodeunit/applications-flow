dc2f8bb1fa5045e2117c0b858bba30e13adb2692
mcgear <michael.gearhardt@gmail.com>
2022-02-08 15:12:27 -0700

    WordPress lcu

--- projects/social-host/src/app/controls/applications/applications.component.ts
+++ projects/social-host/src/app/controls/applications/applications.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Guid } from '@lcu/common';
-import { ApplicationsFlowState, EaCService, SaveApplicationAsCodeEventRequest } from '@lowcodeunit/applications-flow-common';
-import { EaCApplicationAsCode, EaCEnvironmentAsCode, EaCSourceControl } from '@semanticjs/common';
+import {
+  ApplicationsFlowState,
+  EaCService,
+  SaveApplicationAsCodeEventRequest,
+} from '@lowcodeunit/applications-flow-common';
+import {
+  EaCApplicationAsCode,
+  EaCEnvironmentAsCode,
+  EaCSourceControl,
+} from '@semanticjs/common';
 import { EditApplicationFormComponent } from 'projects/common/src/lib/controls/edit-application-form/edit-application-form.component';
 import { ProcessorDetailsFormComponent } from 'projects/common/src/lib/controls/processor-details-form/processor-details-form.component';
 import { SecurityToggleComponent } from 'projects/common/src/lib/controls/security-toggle/security-toggle.component';
 import { SourceControlFormComponent } from 'projects/common/src/lib/controls/source-control-form/source-control-form.component';
 
-
 @Component({
   selector: 'lcu-applications',
   templateUrl: './applications.component.html',
-  styleUrls: ['./applications.component.scss']
+  styleUrls: ['./applications.component.scss'],
 })
 export class ApplicationsComponent implements OnInit {
-
   @ViewChild(EditApplicationFormComponent)
-  public ApplicationFormControls: EditApplicationFormComponent
+  public ApplicationFormControls: EditApplicationFormComponent;
 
   @ViewChild(SecurityToggleComponent)
   public SecurityToggleFormControls: SecurityToggleComponent;
@@ -37,7 +43,9 @@ export class ApplicationsComponent implements OnInit {
   }
 
   public get Environment(): EaCEnvironmentAsCode {
-    return this.State?.EaC?.Environments[this.State?.EaC?.Enterprise?.PrimaryEnvironment];
+    return this.State?.EaC?.Environments[
+      this.State?.EaC?.Enterprise?.PrimaryEnvironment
+    ];
   }
 
   public get DefaultSourceControl(): EaCSourceControl {
@@ -57,53 +65,42 @@ export class ApplicationsComponent implements OnInit {
 
   public ProjectLookup: string;
 
-  constructor(private activatedRoute: ActivatedRoute,
-    protected eacSvc: EaCService,
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    protected eacSvc: EaCService
   ) {
+    this.Stats = [
+      { Name: 'Retention Rate', Stat: '85%' },
+      { Name: 'Bounce Rate', Stat: '38%' },
+      { Name: 'Someother Rate', Stat: '5%' },
+    ];
 
-    this.Stats = [{ Name: "Retention Rate", Stat: "85%" },
-    { Name: "Bounce Rate", Stat: "38%" },
-    { Name: "Someother Rate", Stat: "5%" }];
-
-    this.activatedRoute.params.subscribe(params => {
+    this.activatedRoute.params.subscribe((params) => {
       this.ApplicationLookup = params['appLookup'];
       this.ProjectLookup = params['projectLookup'];
     });
-
   }
 
   public ngOnInit(): void {
-
-    this.handleStateChange().then((eac) => { });
-
+    this.handleStateChange().then((eac) => {});
   }
 
   //  API Methods
 
-  public HandleLeftClickEvent(event: any) {
+  public HandleLeftClickEvent(event: any) {}
 
-  }
+  public HandleRightClickEvent(event: any) {}
 
-  public HandleRightClickEvent(event: any) {
-
-  }
-
-  public HandleSaveFormEvent(formValue: any){
+  public HandleSaveFormEvent(formValue: any) {
     console.log('Recieved Save Event: ', formValue);
     this.SaveApplication();
   }
 
-  public UpgradeClicked() {
+  public UpgradeClicked() {}
 
-  }
-
-  public SettingsClicked() {
-
-  }
+  public SettingsClicked() {}
 
   public SaveApplication(): void {
-
-
     const app: EaCApplicationAsCode = this.Application;
     app.Application = {
       Name: this.ApplicationFormControls.NameFormControl.value,
@@ -112,18 +109,17 @@ export class ApplicationsComponent implements OnInit {
     };
 
     app.LookupConfig.PathRegex = `${this.ApplicationFormControls.RouteFormControl.value}.*`;
-  
 
     switch (app.Processor.Type) {
       case 'DFS':
         //will need to replace with this.RouteFormControl.value if other form added
-        app.Processor.BaseHref = `${this.ApplicationFormControls.RouteFormControl.value}/`.replace(
-          '//',
-          '/'
-        );
+        app.Processor.BaseHref =
+          `${this.ApplicationFormControls.RouteFormControl.value}/`.replace(
+            '//',
+            '/'
+          );
 
-        break;    
-
+        break;
     }
 
     if (!app.LookupConfig.PathRegex.startsWith('/')) {
@@ -139,29 +135,21 @@ export class ApplicationsComponent implements OnInit {
     this.eacSvc.SaveApplicationAsCode(saveAppReq);
   }
 
-  
-
-
-  
-
-
-
   public SaveProcessorDetails(formValue: any): void {
-
     console.log('Recieved Save Event: ', formValue);
 
-    const app: EaCApplicationAsCode =  this.Application;
-    app.LookupConfig.AllowedMethods = this.ProcessorDetailsFormControls.MethodsFormControl?.value
-      ?.split(' ')
-      .filter((v: string) => !!v);
+    const app: EaCApplicationAsCode = this.Application;
+    app.LookupConfig.AllowedMethods =
+      this.ProcessorDetailsFormControls.MethodsFormControl?.value
+        ?.split(' ')
+        .filter((v: string) => !!v);
     app.Processor.Type = this.ProcessorDetailsFormControls.ProcessorType;
-      
 
     switch (app.Processor.Type) {
       case 'DFS':
-
         app.Processor.DefaultFile =
-          this.ProcessorDetailsFormControls.DefaultFileFormControl.value || 'index.html';
+          this.ProcessorDetailsFormControls.DefaultFileFormControl.value ||
+          'index.html';
 
         app.LowCodeUnit = {
           Type: this.ProcessorDetailsFormControls.LCUType,
@@ -175,28 +163,39 @@ export class ApplicationsComponent implements OnInit {
             app.LowCodeUnit.Repository =
               this.ProcessorDetailsFormControls.SourceControlFormControls.RepositoryFormControl.value;
 
-            app.LowCodeUnit.Build = this.ProcessorDetailsFormControls.BuildFormControl.value;
+            app.LowCodeUnit.Build =
+              this.ProcessorDetailsFormControls.BuildFormControl.value;
 
             app.LowCodeUnit.Path =
               this.ProcessorDetailsFormControls.SourceControlFormControls.BuildPathFormControl.value;
             break;
 
           case 'NPM':
-            app.LowCodeUnit.Package = this.ProcessorDetailsFormControls.PackageFormControl.value;
+            app.LowCodeUnit.Package =
+              this.ProcessorDetailsFormControls.PackageFormControl.value;
 
-            app.LowCodeUnit.Version = this.ProcessorDetailsFormControls.VersionFormControl.value;
+            app.LowCodeUnit.Version =
+              this.ProcessorDetailsFormControls.VersionFormControl.value;
+            break;
+
+          case 'WordPress':
+            app.LowCodeUnit.APIRoot =
+              this.ProcessorDetailsFormControls.APIRootFormControl.value;
             break;
 
           case 'Zip':
-            app.LowCodeUnit.ZipFile = this.ProcessorDetailsFormControls.ZipFileFormControl.value;
+            app.LowCodeUnit.ZipFile =
+              this.ProcessorDetailsFormControls.ZipFileFormControl.value;
             break;
         }
         break;
 
       case 'OAuth':
-        app.Processor.Scopes = this.ProcessorDetailsFormControls.ScopesFormControl.value.split(' ');
+        app.Processor.Scopes =
+          this.ProcessorDetailsFormControls.ScopesFormControl.value.split(' ');
 
-        app.Processor.TokenLookup = this.ProcessorDetailsFormControls.TokenLookupFormControl.value;
+        app.Processor.TokenLookup =
+          this.ProcessorDetailsFormControls.TokenLookupFormControl.value;
 
         app.LowCodeUnit = {
           Type: this.ProcessorDetailsFormControls.LCUType,
@@ -204,15 +203,18 @@ export class ApplicationsComponent implements OnInit {
 
         switch (app.LowCodeUnit.Type) {
           case 'GitHubOAuth':
-            app.LowCodeUnit.ClientID = this.ProcessorDetailsFormControls.ClientIDFormControl.value;
+            app.LowCodeUnit.ClientID =
+              this.ProcessorDetailsFormControls.ClientIDFormControl.value;
 
-            app.LowCodeUnit.ClientSecret = this.ProcessorDetailsFormControls.ClientSecretFormControl.value;
+            app.LowCodeUnit.ClientSecret =
+              this.ProcessorDetailsFormControls.ClientSecretFormControl.value;
             break;
         }
         break;
 
       case 'Proxy':
-        app.Processor.InboundPath = this.ProcessorDetailsFormControls.InboundPathFormControl.value;
+        app.Processor.InboundPath =
+          this.ProcessorDetailsFormControls.InboundPathFormControl.value;
 
         app.LowCodeUnit = {
           Type: this.ProcessorDetailsFormControls.LCUType,
@@ -220,26 +222,33 @@ export class ApplicationsComponent implements OnInit {
 
         switch (app.LowCodeUnit.Type) {
           case 'API':
-            app.LowCodeUnit.APIRoot = this.ProcessorDetailsFormControls.APIRootFormControl.value;
+            app.LowCodeUnit.APIRoot =
+              this.ProcessorDetailsFormControls.APIRootFormControl.value;
 
-            app.LowCodeUnit.Security = this.ProcessorDetailsFormControls.SecurityFormControl.value;
+            app.LowCodeUnit.Security =
+              this.ProcessorDetailsFormControls.SecurityFormControl.value;
 
             break;
 
           case 'SPA':
-            app.LowCodeUnit.SPARoot = this.ProcessorDetailsFormControls.SPARootFormControl.value;
+            app.LowCodeUnit.SPARoot =
+              this.ProcessorDetailsFormControls.SPARootFormControl.value;
             break;
         }
         break;
 
       case 'Redirect':
-        app.Processor.IncludeRequest = !!this.ProcessorDetailsFormControls.IncludeRequestFormControl.value;
+        app.Processor.IncludeRequest =
+          !!this.ProcessorDetailsFormControls.IncludeRequestFormControl.value;
 
-        app.Processor.Permanent = !!this.ProcessorDetailsFormControls.PermanentFormControl.value;
+        app.Processor.Permanent =
+          !!this.ProcessorDetailsFormControls.PermanentFormControl.value;
 
-        app.Processor.PreserveMethod = !!this.ProcessorDetailsFormControls.PreserveMethodFormControl.value;
+        app.Processor.PreserveMethod =
+          !!this.ProcessorDetailsFormControls.PreserveMethodFormControl.value;
 
-        app.Processor.Redirect = this.ProcessorDetailsFormControls.RedirectFormControl.value;
+        app.Processor.Redirect =
+          this.ProcessorDetailsFormControls.RedirectFormControl.value;
         break;
     }
 
@@ -256,19 +265,17 @@ export class ApplicationsComponent implements OnInit {
     this.eacSvc.SaveApplicationAsCode(saveAppReq);
   }
 
-
-
   public SaveSecuritySettings(formValue: any): void {
-
     console.log('Recieved Save Event: ', formValue);
 
     const app: EaCApplicationAsCode = this.Application;
-        
-    app.LookupConfig.IsPrivate = this.SecurityToggleFormControls.IsPrivateFormControl.value;
-    app.LookupConfig.IsTriggerSignIn = this.SecurityToggleFormControls.IsPrivateFormControl.value
-        ? this.SecurityToggleFormControls.IsTriggerSignInFormControl.value
-        : false;
-        
+
+    app.LookupConfig.IsPrivate =
+      this.SecurityToggleFormControls.IsPrivateFormControl.value;
+    app.LookupConfig.IsTriggerSignIn = this.SecurityToggleFormControls
+      .IsPrivateFormControl.value
+      ? this.SecurityToggleFormControls.IsTriggerSignInFormControl.value
+      : false;
 
     if (!app.LookupConfig.PathRegex.startsWith('/')) {
       app.LookupConfig.PathRegex = `/${app.LookupConfig.PathRegex}`;
@@ -279,7 +286,6 @@ export class ApplicationsComponent implements OnInit {
       Application: app,
       ApplicationLookup: this.ApplicationLookup || Guid.CreateRaw(),
     };
-
 
     this.eacSvc.SaveApplicationAsCode(saveAppReq);
   }
@@ -307,8 +313,6 @@ export class ApplicationsComponent implements OnInit {
   //   this.eacSvc.SaveApplicationAsCode(saveAppReq);
   // }
 
-
-
   // public SaveApplication(event: string): void {
   //   const app: EaCApplicationAsCode = {
   //       Application: {
@@ -335,8 +339,6 @@ export class ApplicationsComponent implements OnInit {
   //         Type: this.ProcessorDetailsFormControls.ProcessorType,
   //       },
   //     };
-
-  
 
   //   switch (app.Processor.Type) {
   //     case 'DFS':
@@ -448,23 +450,11 @@ export class ApplicationsComponent implements OnInit {
   //   this.eacSvc.SaveApplicationAsCode(saveAppReq);
   // }
 
-
-
   //HELPERS
-
-
-
 
   protected async handleStateChange(): Promise<void> {
     this.State.Loading = true;
 
     await this.eacSvc.EnsureUserEnterprise();
   }
-
-
-
-
-
-
-
 }
