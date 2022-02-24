990272758a4592543f88c015de6e59e5c42840a8
mcgear <michael.gearhardt@gmail.com>
2022-02-23 18:08:11 -0700

    Closer on the build

--- projects/common/src/lib/elements/feed-card-sm/feed-card-sm.component.ts
+++ projects/common/src/lib/elements/feed-card-sm/feed-card-sm.component.ts
@@ -1,42 +1,45 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FeedItem, FeedItemAction } from '../../models/user-feed.model';
 
 @Component({
   selector: 'lcu-feed-card-sm',
   templateUrl: './feed-card-sm.component.html',
-  styleUrls: ['./feed-card-sm.component.scss']
+  styleUrls: ['./feed-card-sm.component.scss'],
 })
 export class FeedCardSmComponent implements OnInit {
-
-  @Input('icon')
-  public Icon: string;
-
-  @Input('title')
-  public Title: string;
-
-  @Input('subtext')
-  public Subtext: string;
-
-  @Input('description')
-  public Description: string;
+  @Input('feed-item')
+  public FeedItem: FeedItem;
 
   public IconColor: string;
 
-  constructor() { }
+  public Icon: string;
 
-  ngOnInit(): void {
-    this.determineIconColor();
+  constructor() {}
+
+  //  Life Cycle
+  public ngOnInit(): void {
+    this.determineIcon();
   }
 
-  // HELPERS
-  protected determineIconColor(): void{
-    if (this.Icon === 'check_circle'){
+  //  API Methods
+  public HandleAction(action: FeedItemAction) {
+    console.log('Action clicked');
+  }
 
+  //  Helpers
+  protected determineIcon(): void {
+    if (this.FeedItem.Status.Code === 0) {
+      this.Icon = 'check_circle';
+      
       this.IconColor = 'green';
-
-    }
-    else if (this.Icon === 'cancel'){
+    } else if (this.FeedItem.Status.Code === 1) {
+      this.Icon = 'cancel';
+      
       this.IconColor = 'red';
+    } else if (this.FeedItem.Status.Code === 2) {
+      this.Icon = 'sync';
+      
+      this.IconColor = 'blue';
     }
   }
-
 }
