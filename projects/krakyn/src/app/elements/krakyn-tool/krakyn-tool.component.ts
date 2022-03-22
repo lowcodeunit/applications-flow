62c49eaba60d9a87fd9530b609b864b6ad98eca5
mcgear <michael.gearhardt@gmail.com>
2022-03-22 14:38:06 -0600

    Local flow importer

--- projects/krakyn/src/app/elements/krakyn-tool/krakyn-tool.component.ts
+++ projects/krakyn/src/app/elements/krakyn-tool/krakyn-tool.component.ts
@@ -7,29 +7,208 @@ import {
   // ApplicationsFlowService,
   ProjectService,
 } from '@lowcodeunit/applications-flow-common';
-import { ConstantUtils, DragDropUtils, DragItemsTemplates, VariablesUtils } from '@semanticjs/krakyn';
-import { EaCNapkinIDEFlowImporter } from '@semanticjs/napkin-ide';
+import { EnterpriseAsCode } from '@semanticjs/common';
+import {
+  ConstantUtils,
+  DragDropUtils,
+  DragItemsTemplates,
+  VariablesUtils,
+} from '@semanticjs/krakyn';
+import {
+  NapkinIDEFlow,
+  NapkinIDEFlowImporter,
+  NapkinIDENode,
+} from '@semanticjs/napkin-ide';
+
+export class EaCNapkinIDEFlowImporter extends NapkinIDEFlowImporter<EnterpriseAsCode> {
+  //  Fields
+
+  //  Properties
+
+  //  Constructors
+  constructor() {
+    super();
+  }
+
+  //  API Methods
+  public Import(eac: EnterpriseAsCode): NapkinIDEFlow {
+    const flow = new NapkinIDEFlow();
+
+    
+    if (eac != null) {
+      flow.Name = eac.Enterprise?.Name;
+
+      flow.ID = eac.EnterpriseLookup;
+
+      this.establishFlowNodesAndEdges(eac, flow);
+    }
+
+    return flow;
+  }
+
+  //  Helpers
+  protected establishFlowNodesAndEdges(
+    eac: EnterpriseAsCode,
+    flow: NapkinIDEFlow
+  ): void {
+    const projLookups = Object.keys(eac.Projects || {});
+
+    let sysCount = 0;
+
+    const requestNode: NapkinIDENode = {
+      ID: `sys-${++sysCount}`,
+      Type: 'request',
+      ClassList: [],
+      Data: {},
+    };
+
+    flow.Nodes = [requestNode];
+
+    flow.Edges = [];
+
+    //  Process for project nodes
+    projLookups.forEach((projLookup: any,index: number) => {
+      const project = eac.Projects![projLookup];
+
+      //  Setup Project Node
+      const projectNode = new NapkinIDENode();
+      projectNode.Type = 'project';
+      projectNode.ID = `${projectNode.Type}-${projLookup}`;
+      projectNode.Data = {
+        Lookup: projLookup,
+        Name: project.Project?.Name,
+        Hosts: project.Hosts,
+      };
+
+      flow.Nodes?.push(projectNode);
+
+      //  Add Request => Project Edge
+      flow.Edges?.push({
+        ID: `sys-edge-${++sysCount}`,
+        NodeInID: requestNode.ID,
+        NodeOutID: projectNode.ID,
+      });
+
+      //  Process for application nodes
+      project.ApplicationLookups?.forEach((appLookup) => {
+        const app = eac.Applications![appLookup];
+
+        //  Setup AppLookupConfig Nodes
+        //  Setup Security Filters
+        // app.AccessRightLookups;
+        // app.LookupConfig.AccessRightsAllAny;
+        // app.LookupConfig.IsPrivate;
+        // app.LookupConfig.IsTriggerSignIn;
+        // app.LookupConfig.LicensesAllAny;
+        // app.LicenseConfigurationLookups;
+
+        //  Setup Route Filters
+        const routeParts = app
+          .LookupConfig!.PathRegex?.split('/')
+          .slice(1)
+          .map((pr) =>  {
+            return `/${pr}`
+          });
+
+        let routePartType = 'path';
+
+        if (app.LookupConfig!.QueryRegex) {
+          routeParts?.push(`?${app.LookupConfig!.QueryRegex!}`);
+
+          routePartType = 'query';
+        }
+
+        if (app.LookupConfig!.HeaderRegex) {
+          routeParts?.push(`[${app.LookupConfig!.HeaderRegex!}]`);
+
+          routePartType = 'header';
+        }
+
+        if (app.LookupConfig!.UserAgentRegex) {
+          routeParts?.push(`#${app.LookupConfig!.UserAgentRegex!}`);
+
+          routePartType = 'user-agent';
+        }
+
+        let pathPartsCount = 0;
+
+        let lastRoutePartNodeId = projectNode.ID;
+
+        //  Setup Route Filters
+        routeParts?.forEach((routePart) => {
+          const routePartType = `route-filter`;
+          const routePartNodeId = `${routePartType}-${pathPartsCount}`;
+
+          let routePartNode = this.loadExistingNode(flow, routePartNodeId);
+
+          if (routePartNode == null) {
+            routePartNode = new NapkinIDENode();
+            routePartNode.Type = routePartType;
+            routePartNode.ID = routePartNodeId;
+            routePartNode.Data = {
+              Name: routePart,
+              Type: routePartType,
+            };
+          }
+
+          //  Add last route part => new route part Edge
+          flow.Edges?.push({
+            ID: `sys-edge-${++sysCount}`,
+            NodeInID: lastRoutePartNodeId,
+            NodeOutID: routePartNode.ID,
+          });
+
+          lastRoutePartNodeId = routePartNodeId;
+
+          pathPartsCount++;
+        });
+
+        //  Setup Application Nodes
+        const appNode = new NapkinIDENode();
+        appNode.Type = 'application';
+        appNode.ID = `${appNode.Type}-${appLookup}`;
+        appNode.Data = {
+          Details: app.Application,
+          Processor: app.Processor,
+        };
+
+        flow.Nodes?.push(appNode);
+
+        //  Add last route part => Application Edge
+        flow.Edges?.push({
+          ID: `sys-edge-${++sysCount}`,
+          NodeInID: lastRoutePartNodeId,
+          NodeOutID: appNode.ID,
+        });
+      });
+    });
+
+    //  Setup DevOps nodes (Source Control and DevOpsActions)...  relate apps to source control via edges
+  }
+
+  protected loadExistingNode(flow: NapkinIDEFlow, id: string): NapkinIDENode {
+    return <NapkinIDENode>flow.Nodes?.find((node) => node.ID === id);
+  }
+}
 
 @Component({
   selector: 'app-krakyn-tool',
   templateUrl: './krakyn-tool.component.html',
-  styleUrls: ['./krakyn-tool.component.scss']
+  styleUrls: ['./krakyn-tool.component.scss'],
 })
 export class KrakynToolComponent implements OnInit {
-
   public State: ApplicationsFlowState;
 
   // Array<{Module: string, Data: Array<any>}>
   public KrakynData: any;
   public SideMenuItems: any;
   public Title: string;
-  public TabMenuItems: Array<{Label: string, Target: string, Class?: string}>;
+  public TabMenuItems: Array<{ Label: string; Target: string; Class?: string }>;
 
   constructor(
     protected injector: Injector,
     protected projectService: ProjectService
   ) {
-
     // super(injector);
 
     this.State = new ApplicationsFlowState();
@@ -42,28 +221,30 @@ export class KrakynToolComponent implements OnInit {
 
     // this.Data = VariablesUtils.DataFlowModuleData[0];
     // console.log('Krakyn Tool Test Data: ', this.Data.Data);
-   }
+  }
 
-   // Lifecycle hooks
+  // Lifecycle hooks
   public ngOnInit(): void {
-
     this.Title = 'The Krakyn Tool';
 
     this.SideMenuItems = DragItemsTemplates.FLOW_DRAG_ITEMS(DragDropUtils.Drag);
 
     this.TabMenuItems = [
-      { Label: 'External Data Test', Target: 'ExternalData', Class: 'selected' },
-      { Label: 'Original Data', Target: 'OriginalData', Class: '' }
-    //   { Label: 'Home', Target: 'Home' }
+      {
+        Label: 'External Data Test',
+        Target: 'ExternalData',
+        Class: 'selected',
+      },
+      { Label: 'Original Data', Target: 'OriginalData', Class: '' },
+      //   { Label: 'Home', Target: 'Home' }
     ];
 
-    this.handleStateChange()
-    .then((eac) => {});
+    this.handleStateChange().then((eac) => {});
   }
 
   /**
    * Krakyn data
-   * 
+   *
    * @returns krakyn data or empty object
    */
   // public KrakynData(): Promise<any> {
@@ -74,31 +255,31 @@ export class KrakynToolComponent implements OnInit {
    * Import tool data
    */
   protected importData(): void {
-
-    const eaCNapkinIDEFlowImporter: EaCNapkinIDEFlowImporter = new EaCNapkinIDEFlowImporter();
+    const eaCNapkinIDEFlowImporter: EaCNapkinIDEFlowImporter =
+      new EaCNapkinIDEFlowImporter();
 
     this.Title = 'Imported data for Krakyn';
 
-    const externalData: { Module: string, Data: any } = {
+    const externalData: { Module: string; Data: any } = {
       Module: 'ExternalData',
-      Data: eaCNapkinIDEFlowImporter.Import(this.State.EaC)
+      Data: eaCNapkinIDEFlowImporter.Import(this.State.EaC),
     };
 
     const dataIndex: number = 0;
 
     VariablesUtils.DataFlowModuleData = [
       ConstantUtils.MapData('ExternalData', externalData.Data),
-      ConstantUtils.MapData('OriginalData', ConstantUtils.ORIGINAL_TEST_DATA)
+      ConstantUtils.MapData('OriginalData', ConstantUtils.ORIGINAL_TEST_DATA),
     ];
 
-    VariablesUtils.ActiveModule = VariablesUtils.DataFlowModuleData[dataIndex].Module;
+    VariablesUtils.ActiveModule =
+      VariablesUtils.DataFlowModuleData[dataIndex].Module;
 
     this.KrakynData = VariablesUtils.DataFlowModuleData[dataIndex];
     debugger;
   }
 
   protected async handleStateChange(): Promise<void> {
-
     this.State.Loading = true;
 
     await this.projectService.HasValidConnection(this.State);
@@ -108,14 +289,11 @@ export class KrakynToolComponent implements OnInit {
     await this.projectService.ListEnterprises(this.State);
 
     if (this.State.Enterprises?.length > 0) {
-
       this.State.Loading = false;
 
       this.importData();
 
       await this.projectService.GetActiveEnterprise(this.State);
-
     }
   }
 }
-
