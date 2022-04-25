47488c9d2dde848945ecad14cad735ae4abd98be
powhound4 <george.hatch@fathym.com>
2022-04-25 13:21:47 -0600

    ellipsis logic

--- projects/social-host/src/app/controls/projects/projects.component.ts
+++ projects/social-host/src/app/controls/projects/projects.component.ts
@@ -211,7 +211,8 @@ export class ProjectsComponent implements OnInit {
             width: '600px',
             data: {
                 hosts: this.State.EaC.Hosts,
-                primaryHost: this.Project?.Hosts[this.Project?.Hosts?.length - 1],
+                primaryHost:
+                    this.Project?.Hosts[this.Project?.Hosts?.length - 1],
                 project: this.Project,
                 projectLookup: this.ProjectLookup,
             },
