d62e2cfbafea5d6ecac06e9b11503dce6788d4a5
powhound4 <george.hatch@fathym.com>
2022-07-12 13:22:24 -0600

    iframe changes

--- projects/common/src/lib/elements/project-wizard-card/project-wizard-card.component.ts
+++ projects/common/src/lib/elements/project-wizard-card/project-wizard-card.component.ts
@@ -9,4 +9,45 @@ export class ProjectWizardCardComponent implements OnInit {
     constructor() {}
 
     ngOnInit(): void {}
+
+    ngAfterViewInit() {
+        this.iframeLoaded();
+    }
+
+    public iframeLoaded() {
+        console.log('hello');
+        var iFrameID = document?.querySelector('#wizard') as HTMLIFrameElement;
+        if (iFrameID) {
+            iFrameID.style.height = '0px';
+        }
+
+        // setTimeout(() => {
+
+        //     // console.log("header = ",iFrameID.contentWindow.document.body.getElementsByClassName('css-3gkkwt') )
+
+        //     const els = iFrameID.contentWindow.document.body.getElementsByClassName('css-3gkkwt');
+        //     if (els.length > 0) {
+        //         els[0].setAttribute("style","display:none;");
+        //     }
+
+        // }, 2000)
+
+        setTimeout(() => {
+            if (iFrameID) {
+                const els =
+                    iFrameID.contentWindow.document.body.getElementsByClassName(
+                        'css-3gkkwt'
+                    );
+                if (els.length > 0) {
+                    els[0].setAttribute('style', 'display:none;');
+                }
+                // here you can make the height, I delete it first, then I make it again
+                iFrameID.style.height = '';
+                iFrameID.style.height =
+                    iFrameID.contentWindow.document.body.offsetHeight +
+                    40 +
+                    'px';
+            }
+        }, 5000);
+    }
 }
