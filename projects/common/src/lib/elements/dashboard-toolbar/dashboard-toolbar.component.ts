2160d0ea09a6ce52fb6f2f6d548b923b7dba72e8
powhound4 <george.hatch@fathym.com>
2022-07-21 11:38:33 -0600

    tweaks

--- projects/common/src/lib/elements/dashboard-toolbar/dashboard-toolbar.component.ts
+++ projects/common/src/lib/elements/dashboard-toolbar/dashboard-toolbar.component.ts
@@ -1,5 +1,7 @@
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { EaCService } from '../../services/eac.service';
+import { ApplicationsFlowState } from '../../state/applications-flow.state';
 
 @Component({
     selector: 'lcu-dashboard-toolbar',
@@ -7,8 +9,21 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./dashboard-toolbar.component.scss'],
 })
 export class DashboardToolbarComponent implements OnInit {
+    public get State(): ApplicationsFlowState {
+        return this.eacSvc.State;
+    }
+
+    public get ProjectLookups(): string[] {
+        // console.log("PJS: ",Object.keys(this.State?.EaC?.Projects || {}).reverse() )
+        return Object.keys(this.State?.EaC?.Projects || {}).reverse();
+    }
+
     public IsSmScreen: boolean;
-    constructor(public breakpointObserver: BreakpointObserver) {}
+
+    constructor(
+        public breakpointObserver: BreakpointObserver,
+        protected eacSvc: EaCService
+    ) {}
 
     ngOnInit(): void {
         this.breakpointObserver
